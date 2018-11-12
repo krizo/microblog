e9f2cd761604370ce268933126144bf5962613f0
Krzysztof Bober <krzysztof.bober@getbase.com>
2018-11-12 22:21:54 +0100

    Introducing templates

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, flash, url_for
+
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -16,4 +18,13 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template(url_for('index'), title='Home', user=user, posts=posts)
+
+
+@app.route('/login',  methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect(url_for('login'))
+    return render_template('login.html', title='Sign In', form=form)
